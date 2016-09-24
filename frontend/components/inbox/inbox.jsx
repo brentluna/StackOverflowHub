8ee2391edd0b71f9470953880afbe2228b4f2d9f
530farm <brentadamluna@gmail.com>
2016-09-23 21:54:57 -0700

    added date to inbox

--- frontend/components/inbox/inbox.jsx
+++ frontend/components/inbox/inbox.jsx
@@ -15,10 +15,16 @@ class Inbox extends React.Component {
         if (el.is_unread) {
           read = 'inbox-li unread';
         }
+        let dateInt = parseInt(`${el.creation_date}000`)
+        let date = new Date(dateInt)
+        date = date.toDateString()
         lis.push(
           <a href={el.link} target='_blank'>
             <li className={read} >
               {el.title}
+              <br/>
+              <br/>
+              {date}
             </li>
           <hr className='search-hr'/>
           </a>
