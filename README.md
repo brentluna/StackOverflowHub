3750a5cce921364e600c5cf51b94a418ae093003
530farm <brentadamluna@gmail.com>
2016-09-25 14:34:06 -0700

    removed some methods placed on the window

---
+++ README.md
@@ -0,0 +1,6 @@
+# StackOverflow Hub 
+[Live Site](https://chrome.google.com/webstore/detail/stackoverflow-hub/bolpmjgkepeegcbpojocngghpjofckol)
+
+### Description
+
+A handy tool for any programmer. A popup chrome extension that provides you quick access to one of the greatest resources for programmers, with out having to open up a new browser tab and interrupting your workflow. It provides a search feature with autocomplete results as well as access to your inbox. 
--- frontend/components/so_hub.jsx
+++ frontend/components/so_hub.jsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import Root from './root';
 import configureStore from '../store/store';
 
-import {fetchSearch, fetchAuth, ajaxAuth} from '../util/api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.store = store;
-  window.fetchAuth = fetchAuth;
-  window.ajaxAuth = ajaxAuth;
-  window.fetchSearch = fetchSearch;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 
